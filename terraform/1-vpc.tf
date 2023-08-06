3c58dadbcbcb667c4f2b2b79f3ddfa857f794c8f
victor.katte <victor.katte@totalcloudconnect.com>
2023-08-06 19:59:10 +0100

    fix the instances

--- terraform/1-vpc.tf
+++ terraform/1-vpc.tf
@@ -16,9 +16,10 @@ module "vpc" {
     "kubernetes.io/role/internal-elb" = "1"
   }
 
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
+  enable_nat_gateway     = false
+  single_nat_gateway     = false
   one_nat_gateway_per_az = false
+  map_public_ip_on_launch = true
 
   enable_dns_hostnames = true
   enable_dns_support   = true
